0f8ea4b0d8a587df510891aaddd481a978b78166
chucknerd <chucknerd1@gmail.com>
2019-12-13 06:01:18 -0600

    JS

--- script.js
+++ script.js
@@ -3,26 +3,42 @@
 /**app's state (variables) */
 let board;
 
+let turn = "X";
+
 
 /**cached element references */
 
+const squares =
+Array.from(document.querySelectorAll("#board div"));
+
 /**event listeners */
+
+document.getElementById("board").addEventListener("click", handleTurn);
 
 /**functions */
 function init() {
     board = [
-        "1", "", "",
+        "", "", "",
         "", "", "",
         "", "", "",
     ];
+
+    render();
 };
 
-render();
+
 
 function render() {
     board.forEach(function(mark, index){
-        console.log(mark, index)
+        squares[index].textContent = mark;
     });
 };
+
+function handleTurn(event) {
+    let idx = squares.findIndex(function(square) {
+        
+        return square === event.target;
+    });
+}
 
 init();
